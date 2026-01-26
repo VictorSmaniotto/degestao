99d9d40148faf687510f29d7bf3638a73fdef0fc
VictorSmaniotto <victorsmaniotto@hotmail.com>
2026-01-25 23:31:29 -0300

    Fix: upgrade init-letsencrypt.sh to support docker compose v2

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
+# Detect docker-compose command
+if command -v docker-compose &> /dev/null; then
+    DC_CMD="docker-compose"
+elif docker compose version &> /dev/null; then
+    DC_CMD="docker compose"
+else
+    echo 'Error: docker-compose nor "docker compose" is installed.' >&2
+    exit 1
 fi
 
 domains=(degestao.app www.degestao.app)
@@ -30,7 +35,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+$DC_CMD -f docker-compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +44,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose -f docker-compose.prod.yml up --force-recreate -d web
+$DC_CMD -f docker-compose.prod.yml up --force-recreate -d web
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+$DC_CMD -f docker-compose.prod.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -66,7 +71,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
+$DC_CMD -f docker-compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -77,4 +82,4 @@ docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose -f docker-compose.prod.yml exec web nginx -s reload
+$DC_CMD -f docker-compose.prod.yml exec web nginx -s reload
